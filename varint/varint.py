89b0b3820305533f3894909354a8cae0489b93e4
Pieter Arthur de Jong <pieter.a.dejong@gmail.com>
2023-05-01 15:02:14 -0400

    add solution skeleton

---
+++ varint/varint.py
@@ -0,0 +1,17 @@
+class Solution:
+    def __init__(self):
+        pass
+
+    def encode(self):
+        pass
+
+    def decode(self):
+        pass
+
+def main():
+    pass
+    # assert encode(150) == b'\x96\x01'
+    # assert decode(b'\x96\x01') == 150
+
+if __name__ == "__main__":
+    main()
